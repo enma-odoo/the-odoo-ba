92ec7374c05448fc26cab870cdb6665c9c18f339
Enock <enma@odoo.com>
2026-04-06 16:24:57 +0300

    [ADD] estate: create estate.property model and basic fields

---
+++ tutorials/estate/models/estate_property.py
@@ -0,0 +1,27 @@
+# -*- coding: utf-8 -*-
+
+from odoo import models, fields
+
+class EstateProperty(models.Model):
+    # _name defines the name of the database table (replaces '.' with '_', so it becomes estate_property)
+    _name = "estate.property"
+    _description = "Real Estate Property"
+
+    # Basic Fields
+    name = fields.Char(required=True)
+    description = fields.Text()
+    postcode = fields.Char()
+    date_availability = fields.Date(copy=False, default=fields.Date.today)
+    expected_price = fields.Float(required=True)
+    selling_price = fields.Float(readonly=True, copy=False)
+    bedrooms = fields.Integer(default=2)
+    living_area = fields.Integer()
+    facades = fields.Integer()
+    garage = fields.Boolean()
+    garden = fields.Boolean()
+    garden_area = fields.Integer()
+    garden_orientation = fields.Selection(
+        string='Type',
+        selection=[('north', 'North'), ('south', 'South'), ('east', 'East'), ('west', 'West')],
+        help="Garden orientation"
+    )
