19b0448c02cdeb6b88b8669c143e5c87ad77d722
Enock <enma@odoo.com>
2026-04-06 18:10:32 +0300

    [ADD] estate: define window action and menus for estate.property

--- tutorials/estate/__manifest__.py
+++ tutorials/estate/__manifest__.py
@@ -20,6 +20,8 @@ This module allows you to:
     ],
     'data': [
         'security/ir.model.access.csv',
+        'views/estate_property_views.xml',
+        'views/estate_menus.xml',
     ],
     'installable': True,
     'application': True,
