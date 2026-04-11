1a9e1deb460f5a1b479a20c2fd734f008ea82808
Enock <enma@odoo.com>
2026-04-11 09:28:59 +0300

    [ADD] estate: create property.tag model and many2many relation

--- tutorials/estate/models/estate_property.py
+++ tutorials/estate/models/estate_property.py
@@ -34,4 +34,6 @@ class EstateProperty(models.Model):
         ('canceled', 'Canceled')
     ], required=True, copy=False, default='new')
     property_type_id = fields.Many2one("estate.property.type", string="Property Type")
-    
+    salesperson_id = fields.Many2one("res.users", string="Salesperson", default=lambda self: self.env.user)
+    buyer_id = fields.Many2one("res.partner", string="Buyer", copy=False)
+    tag_ids = fields.Many2many("estate.property.tag", string="Tags")
